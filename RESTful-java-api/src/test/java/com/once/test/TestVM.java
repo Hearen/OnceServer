38fe164cffc20893c6e2dbf1d69f5cd6fa3aedd4
LHearen <lhearen@126.com>
2015-12-31 17:26:26 +0800

    API updated

--- RESTful-java-api/src/test/java/com/once/test/TestVM.java
+++ RESTful-java-api/src/test/java/com/once/test/TestVM.java
@@ -8,7 +8,6 @@ public class TestVM {
 
 	public static void main(String[] args) throws IOException {
 		VMConfig config = new VMConfig("vm", 2, 1024, "/home/res/images/test1.qcow2", "/home/res/iso/CentOS-7.1.iso", "ovs0");
-		
 		String uuidString = "2a230355-5e06-4d1e-9e57-9cf940571c03";
 		config.setUUID(uuidString);
 		VM.create(config);
